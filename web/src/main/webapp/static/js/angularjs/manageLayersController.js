a4b40a962590f63c6ce7c2dd9cb2b9b6091b89ba
doublebyte1 <doublebyte@gmail.com>
2017-03-30 17:24:54 +0200

    - Added smart-table filtering, sorting, pagination. - Added links on datasetname

--- web/src/main/webapp/static/js/angularjs/manageLayersController.js
+++ web/src/main/webapp/static/js/angularjs/manageLayersController.js
@@ -12,48 +12,26 @@
 			});
 		}
 	])
-/*
-    .controller('ManageLayersCtrl', ['$scope', function (scope) {
-        scope.rowCollection = [
-            {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
-            {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
-            {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
-        ];
-
-
-    }]);
-*/
-
 
 		.controller('ManageLayersCtrl', function($scope, $http) {
 
-            //$scope.itemsByPage=5;
+            $scope.itemsByPage = 5;
+            $scope.itemsByPage = 7;
             $scope.jsonresult = [];
             $scope.displayedCollection = [].concat($scope.jsonresult);
 
 
             $http({
                 method : "GET",
-                url : "http://localhost:8083/workspaces/sf/datasets?page=1&pageSize=10",
+                url : "http://localhost:8083/workspaces/sf/datasets?page=1&pageSize=600",
                 isArray: true
             }).then(function mySucces(response) {
                 $scope.jsonresult = response.data;
+                //$scope.dispPages = Math.round(response.data.totalElements/$scope.itemsByPage + 0.5);
             }, function myError(response) {
                 $scope.jsonresult = response.statusText;
             });
 
-            $scope.getters={
-                name: function (value) {
-                    //this will sort by the length of the first name string
-                    return value[0].length;
-                }
-            }
-
-            /*
-                          $scope.layerDetails = function() {
-                              ngDialog.open("http://localhost:8083/workspaces/db/datasets/cb_2015_01_bg_500k");
-                          };
-            */
 
 
 
