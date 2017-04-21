0367ad6a238e19fbad39032b1d812fe67846c6d8
doublebyte1 <doublebyte@gmail.com>
2017-04-21 09:49:19 +0200

    - Added confirmation dialog on layer delete

--- web/src/main/webapp/static/js/angularjs/manageLayersController.js
+++ web/src/main/webapp/static/js/angularjs/manageLayersController.js
@@ -30,22 +30,18 @@
                     case $translate("MANAGE_LAYERS.DOWNLOAD"):
                         console.log("download " + "http://localhost:8083/workspaces/"+
                             $scope.ws + "/datasets/" + $scope.ds + "/download");
-
                     case $translate("MANAGE_LAYERS.UPDATE"):
-
+                            //TODO: move this to the upload tab ?
                     case $translate("MANAGE_LAYERS.DELETE"):
-
-                }
-
-
-
+                        confirmDlg();
+                        }
 
             };
 
             $http({
                 method : "GET",
                 //url : "http://localhost:8083/allDatasets",
-                url : "http://localhost:8083/workspaces/db/datasets?pageSize=200",
+                url : "http://localhost:8083/workspaces/db/datasets",
                 isArray: true
             }).then(function mySuccess(response) {
                 $scope.jsonresult = response.data;
@@ -86,8 +82,98 @@
             }
 
 
+            function confirmDlg (options) {
+                var deferredObject = $.Deferred();
+                var defaults = {
+                    type: "confirm", //alert, prompt,confirm
+                    modalSize: 'modal-sm', //modal-sm, modal-lg
+                    okButtonText: 'Ok',
+                    cancelButtonText: 'Cancel',
+                    yesButtonText: 'Yes',
+                    noButtonText: 'No',
+                    headerText: "You are about to remove '" + $scope.ws + ":" + $scope.ds + "'",
+                    messageText: 'Are you sure you want to remove this dataset?',
+                    alertType: 'danger', //default, primary, success, info, warning, danger
+                }
+                $.extend(defaults, options);
+
+                var _show = function(){
+                    var headClass = "alert-danger";
+                    $('BODY').append(
+                        '<div id="ezAlerts" class="modal fade">' +
+                        '<div class="modal-dialog" class="' + defaults.modalSize + '">' +
+                        '<div class="modal-content">' +
+                        '<div id="ezAlerts-header" class="modal-header ' + headClass + '">' +
+                        '<button id="close-button" type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>' +
+                        '<h4 id="ezAlerts-title" class="modal-title">Modal title</h4>' +
+                        '</div>' +
+                        '<div id="ezAlerts-body" class="modal-body">' +
+                        '<div id="ezAlerts-message" ></div>' +
+                        '</div>' +
+                        '<div id="ezAlerts-footer" class="modal-footer">' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>'
+                    );
+
+                    $('.modal-header').css({
+                        'padding': '15px 15px',
+                        '-webkit-border-top-left-radius': '5px',
+                        '-webkit-border-top-right-radius': '5px',
+                        '-moz-border-radius-topleft': '5px',
+                        '-moz-border-radius-topright': '5px',
+                        'border-top-left-radius': '5px',
+                        'border-top-right-radius': '5px'
+                    });
+
+                    $('#ezAlerts-title').text(defaults.headerText);
+                    $('#ezAlerts-message').html(defaults.messageText);
+
+                    var keyb = "false", backd = "static";
+                    //var calbackParam = "";
+
+                    var btnhtml = '<button id="ezok-btn" class="btn btn-primary">' + defaults.noButtonText + '</button>';
+
+                    if (defaults.yesButtonText && defaults.yesButtonText.length > 0) {
+                        btnhtml += '<button id="ezclose-btn" class="btn btn-default">' + defaults.yesButtonText + '</button>';
+                    }
+                    $('#ezAlerts-footer').html(btnhtml).on('click', 'button', function (e) {
+                        if (e.target.id === 'ezok-btn') {//false
+                            //calbackParam = false;
+                            //console.log("cancel");
+                            $('#ezAlerts').modal('hide');
+                        } else if (e.target.id === 'ezclose-btn') {//true
+                            //calbackParam = true;
+                            //console.log("ok");
+                            $('#ezAlerts').modal('hide');
+                        }
+                        //$('#ezAlerts').modal('hide');
+                        //console.log(calbackParam);
+                    });
+
+                    $('#ezAlerts').modal({
+                        show: false,
+                        backdrop: backd,
+                        keyboard: keyb
+                    }).on('hidden.bs.modal', function (e) {
+                        $('#ezAlerts').remove();
+                        deferredObject.resolve(calbackParam);
+                    }).on('shown.bs.modal', function (e) {
+                        if ($('#prompt').length > 0) {
+                            $('#prompt').focus();
+                        }
+                    }).modal('show');
+                }
+
+                _show();
+                return deferredObject.promise();
+            }
+
+
 
 		})
+
 
     .controller('PopupCtrl', ['$scope','$modalInstance', 'jsonresp', function ($scope, $modalInstance, jsonresp) {
         $scope.details = jsonresp.data;
@@ -98,6 +184,7 @@
         };
 
     }]);
+
 
 
 
